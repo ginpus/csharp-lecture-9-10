9da2b0b70de139107559ae461a33e7e469fb7a8d
ginpus <gintautas.pusinskas@gmail.com>
2021-07-12 19:27:38 +0300

    Initial commit

---
+++ lecture9/Person.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lecture9
+{
+    internal class Person
+    {
+        private string Name;
+        private string SurName;
+        private int Age;
+
+        public Person(string name, string surname, int age)
+        {
+            Name = name;
+            SurName = surname;
+            Age = age;
+        }
+
+        public void SetName(string name)
+        {
+            Name = name;
+            SayName(name);
+        }
+
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public void SayName(string name)
+        {
+            Console.WriteLine(name);
+        }
+    }
+}
---
+++ lecture9/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace lecture9
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var pranas = new Person("Pranas", "Masiotas", 52);
+
+            pranas.SetName("Algis");
+        }
+    }
+}
